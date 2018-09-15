ce0901a74091fce977d6e43e47b0afd133c7d343
Aniket Hemant Shinde <aniketshinde.work@gmail.com>
2018-09-15 16:26:04 +0530

    two modules done user and question

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/petShop');
+mongoose.connection.on('error',function(){
+    console.log('error in mongo connection');
+})
+mongoose.connection.on('open',function(){
+    console.log('connected to mongo');
+})
+
+
+
+const userController = require('./controllers/user');
+const questionController = require('./controllers/question');
+
+
+
 
 const app = express();
 
@@ -14,6 +31,18 @@ app.get('/',function(req,res){
     res.send('Hello World');
 });
 
+
+
+
+app.get('/api/v1/users', userController.getAllUsers)
+app.post('/api/v1/users', userController.postNewUsers)
+app.put('/api/v1/users/:id', userController.updateUsersById)
+app.delete('/api/v1/users/:id', userController.delUsersById)
+
+app.get('/api/v1/questions', questionController.getAllQuestions)
+app.post('/api/v1/questions', questionController.postNewQuestions)
+app.put('/api/v1/questions/:id',questionController.updateQuestionById)
+app.delete('/api/v1/questions/:id', questionController.delQuestionById)
 
 
 app.set('port',3000);
